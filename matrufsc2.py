edf1b8c14d8acf091cca87814118ad93615185f4
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-04-19 00:14:18 -0300

    Added static files routes to WSGI app to allow the fast test of it

--- matrufsc2.py
+++ matrufsc2.py
@@ -1,13 +1,34 @@
-from bottle import run, default_app, debug
+from bottle import run, default_app, debug, request, response, route, static_file 
 import os
-application = default_app() # For Google App Engine recognize the request
 
-# We yet does not use this file
-# But it will be used when I put cron jobs (which Google App Engine will call) here
-# And too when I put an API here =)
+IN_DEV = "dev" in os.environ.get("SERVER_SOFTWARE", "").lower() or os.environ.has_key("DEV")
+application = default_app()
 
+@route('/ping.cgi', method='POST')
+def ping():
+	content_disposition = 'attachment; filename=' + request.GET['q']
+	data = request.forms['ping']
+	response.set_header('Content-Type', 'application/octet-stream')
+	response.set_header('Content-Disposition', content_disposition)
+	return data
+
+@route('/css/<path:re:.*>')
+def css(path):
+	return static_file(path, root='./static/css')
+
+@route('/js/<path:re:.*>')
+def js(path):
+	return static_file(path, root='./static/js')
+
+@route('/<path:re:.*>.json')
+def dados(path):
+	return static_file(str(path)+'.json', root='./static/dados')
+
+@route('<:re:.*>')
+def index():
+	return static_file('index.html', root='./static')
 
 # Enables development if running on dev app server
-debug("dev" in os.environ.get("SERVER_SOFTWARE", "").lower() or os.environ.has("DEV"))
+debug(IN_DEV)
 if __name__ == '__main__':
-	run(port=8080, host='localhost')
+	run(port=8181, host='localhost', reloader=IN_DEV)
